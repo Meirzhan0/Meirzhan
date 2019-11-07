1becb19efd4e7f962fd1c13ffbe042d0432e1727
mikola-s <suharev.nv@gmail.com>
2019-11-07 21:53:21 +0200

    lsn21 change 3.14.js

--- 3.14.js
+++ 3.14.js
@@ -1,6 +1,6 @@
 // 3.14. В переменной date лежит дата в формате '2025-12-31'. 
 // Преобразуйте эту дату в формат '31/12/2025'.
 
-let startDate = '2025-12-31'
-let resultDate = startDate.slice(8,10) + '/' + startDate.slice(5,7) + '/' + startDate.slice(0,4)
-console.log(resultDate)
+let str = '2025-12-31'
+
+console.log(str.replace(/(\d{4})-(\d{2})-(\d{2})/, '$3/$2/$1'))
