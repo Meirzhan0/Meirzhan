a13592d323329705359eb775789f3cccc0ce8e5d
mikola-s <suharev.nv@gmail.com>
2019-11-07 13:33:35 +0200

    lsn21 complete 2.07.js

--- 2.06.js
+++ 2.06.js
@@ -4,7 +4,4 @@
 let a = 10
 let b = 2
 
-alert(`a + b = ${a + b} /n
-        a - b = ${a - b} /n 
-        a * b = ${a * b} /n 
-        a // b = ${a / b}`)
+alert(` a + b = ${a + b} \n a - b = ${a - b} \n a * b = ${a * b} \n a / b = ${a / b}`)
