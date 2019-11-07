d06ad52ca78d88436778bb3dd4c15847f9249141
mikola-s <suharev.nv@gmail.com>
2019-11-07 20:44:04 +0200

    lsn21 complete 3.19.js

---
+++ 3.19.js
@@ -0,0 +1,5 @@
+// 3.19. Дана строка 'я учу javascript!'. Найдите позицию подстроки 'учу'.
+
+let str = 'я учу javascript!'
+
+console.log(`позиция 'учу' в подстроке 'я учу javascript!' -- ${str.indexOf('учу')}`)
