4d6bc86fab3f9a4deea3d3a67860f2592fbf37e4
mikola-s <suharev.nv@gmail.com>
2019-11-07 19:52:41 +0200

    lsn21 complete 3.17.js

---
+++ 3.17.js
@@ -0,0 +1,6 @@
+// 3.17. Дана строка 'я учу javascript!'. 
+// Найдите количество символов в этой строке
+
+let str = 'я учу javascript!'
+
+console.log(`длинна строки 'я учу javascript!' -- ${str.length}`)
