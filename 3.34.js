dab4aec77a3e6157781a50f4ffcbdabfec7e5fea
mikola-s <suharev.nv@gmail.com>
2019-11-08 18:56:26 +0200

    lsn21 complete 3.34.js

---
+++ 3.34.js
@@ -0,0 +1,16 @@
+// 3.34. Используя вложенные циклы, сформируйте двумерный массив, содержащий таблицу умножения:
+//
+//  "1x1=1; 2x1=1"
+//  "1x2=2; 2x2=4"
+//
+// И выходим и останавливаем цикл, когда будет 6 умножнить на 6
+
+let arr = new Array(6).fill(new Array(6).fill(''))
+
+for (let indexI = 0; indexI < 6; indexI++) {
+    for (let indexJ = 0; indexJ < 6; indexJ++) {
+        arr[indexI][indexJ] = `${indexJ+1}x${indexI+1}=${(indexJ+1)*(indexJ+1)};`;   
+    }
+}
+
+console.log(arr)
