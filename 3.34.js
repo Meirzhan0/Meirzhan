47e1e87e2c68fa5603097f5b00ba849eac930e87
mikola-s <suharev.nv@gmail.com>
2019-11-11 20:09:26 +0200

    lsn21 correction 3.34.js

--- 3.34.js
+++ 3.34.js
@@ -5,11 +5,12 @@
 //
 // И выходим и останавливаем цикл, когда будет 6 умножнить на 6
 
-let arr = new Array(6).fill(new Array(6).fill(''))
+let arr = new Array(6).fill('').map( () => Array(6).fill(''))
+
 
 for (let indexI = 0; indexI < 6; indexI++) {
     for (let indexJ = 0; indexJ < 6; indexJ++) {
-        arr[indexI][indexJ] = `${indexJ+1}x${indexI+1}=${(indexJ+1)*(indexJ+1)};`;   
+        arr[indexI][indexJ] = `${indexJ+1}x${indexI+1}=${(indexI+1)*(indexJ+1)};`;   
     }
 }
 
